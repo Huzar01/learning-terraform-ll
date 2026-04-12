26492fdf49453dfb6c92385c3bd6b1414b58ae93
Kamil Tulik <huzar01x@gmail.com>
2026-04-12 19:21:44 -0400

    Fixed typo in main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "blog" {
   # vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subtnet_id = module.blog_vpc.public_subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform Blog"
