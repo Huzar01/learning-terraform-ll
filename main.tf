a3875d512bfb9c08daee5ad53c629c37d263cc71
Kamil Tulik <huzar01x@gmail.com>
2026-04-12 18:49:35 -0400

    Create security group

--- main.tf
+++ main.tf
@@ -14,11 +14,53 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform Blog"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog_sg"
+  description = "Allow HTTP traffic in. Allow all out"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "allow_http_in" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.blog.id
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+resource "aws_security_group_rule" "allow_https_in" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.blog.id
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+resource "aws_security_group_rule" "block_all_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.blog.id
+  cidr_blocks       = ["0.0.0.0/0"]
+
 }
